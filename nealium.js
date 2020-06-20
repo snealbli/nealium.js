8b04e9af2dba5dd695f4ce330d11a6e90fd8f383
Sam Neal <sam@nealblim.com>
2020-06-20 11:20:27 -0400

    Initial commit (empty; header-only)

---
+++ nealium.js
@@ -0,0 +1,18 @@
+/* ╔══════════════════════════════════════╦═════════════════════════╦══════════╗
+ * ║ nealium.js                           ║ Created:    5 Jun. 2020 ║ v1.0.0.0 ║
+ * ║                                      ║ Last mod.: 20 Jun. 2020 ╚══════════╣
+ * ╠══════════════════════════════════════╩════════════════════════════════════╣
+ * ║ Javascript library of commonly-used functions, classes, and other         ║
+ * ║ utility.                                                                  ║
+ * ╠═══════════════════════════════════════════════════════════════════════════╣
+ * ║ For the latest version of this, to report a bug, or to contribute, please ║ 
+ * ║ visit:     github.com/snealbli/nealium.js                                 ║
+ * ║    or:     code.nealblim.com/nealium.js                                   ║
+ * ╠═══════════════════════════════════════════════════════════════════════════╣
+ * ║                        by Samuel 'teer' Neal-Blim                         ║
+ * ║                                                                           ║
+ * ║                         Site: prog.nealblim.com                           ║
+ * ║                         Git:  github.com/snealbli                         ║
+ * ║                    JSfiddle:  jsfiddle.net/user/teeer                     ║
+ * ╚═══════════════════════════════════════════════════════════════════════════╝
+ */
